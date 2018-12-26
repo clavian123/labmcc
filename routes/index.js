12ebea21b5028c532cf060d9a4054c8e6159b13b
Clavian Candrian <clavian123@gmail.com>
2018-12-26 10:42:34 +0700

    bikin tampilan & npm install

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router(); 
+var body_parser = require('body-parser')
+
+var passport = require('passport')
+
+var jquery = require('jquery');
+jquery.mobile = require('jquery-mobile');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
